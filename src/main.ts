9afe2e9ae1181ff1a67ad49eff3f07fb33f8d0e6
Augusto Seabra <augusto.seabra00@gmail.com>
2023-09-14 00:12:16 -0300

    fix: build fixes

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+declare var  __webpack_public_path__: string;
+__webpack_public_path__ = '/portfolio-angular/';
